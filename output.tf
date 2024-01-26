40cfb6ffb13df4e99deecac18535af8b3fd586d5
Megadrive91 <157036616+Megadrive91@users.noreply.github.com>
2024-01-26 16:32:23 +0000

    TEST

--- output.tf
+++ output.tf
@@ -4,7 +4,7 @@
 # }
 
 
-# output "network_interface_private_ip" {
-#   description = "private ip addresses of the vm nics"
-#   value       = azurerm_network_interface.vm.*.private_ip_address
-# }
+ output "network_interface_private_ip" {
+   description = "private ip addresses of the vm nics"
+   value       = azurerm_network_interface.vm.*.private_ip_address
+ }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "prefix" {
-  default = "dan-peacock"
+  default = "shafiq_ahmed"
 }
 
 variable "vm_size" {
     description = "Value of the vm_size for the azure VM"
     type = string
-    default = "Standard_DS1_v2"
+    default = "Standard_A1"
 }
 
 variable "default_password" {
